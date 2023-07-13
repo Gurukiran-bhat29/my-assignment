5da61420e1544a31ce5f078dce1c53854b901c3f
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-07-13 23:16:37 +0530

    redux store changes

--- src/utils/store.js
+++ src/utils/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import listSlice from "./listSlice";
 import userSlice from "./userSlice";
 import popUpSlice from "./popUpSlice";
-import chatSlice from "./chatSlice";
 
 const store = configureStore({
   reducer: {
     lists: listSlice,
     user: userSlice,
-    popUp: popUpSlice,
-    chat: chatSlice
+    popUp: popUpSlice
   }
 });
 
